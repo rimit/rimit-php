1d8425cc658de8ed350f7d800634efe8673f1a5d
Rimit <106008521+rimit@users.noreply.github.com>
2024-02-22 10:45:53 +0530

    Update config.php

--- utilities/config.php
+++ utilities/config.php
@@ -2,11 +2,11 @@
 
 date_default_timezone_set("Asia/Kolkata");
 
-// FOR MORE INFO CHECK THE DOCUMENT - https://doc.rimit.co/getting-started/readme#rest 
-define('BASE_URL', 'https://uat-gateway.rimit.co/api/client/rimit/v1'); // FOR UAT API
-//define('BASE_URL', 'https://api-gateway.rimit.co/api/client/rimit/v1'); // FOR PRODUCTION API
+// FOR MORE INFO CHECK THE DOCUMENT - https://developer.rimit.in/getting-started/readme
+define('BASE_URL', 'https://uat-gateway.rimit.in/api/client/rimit/v1'); // FOR UAT API
+//define('BASE_URL', 'https://api-gateway.rimit.in/api/client/rimit/v1'); // FOR PRODUCTION API
 
-// FOR MORE INFO CHECK THE DOCUMENT - https://doc.rimit.co/getting-started/readme#multi-tenant
+// FOR MORE INFO CHECK THE DOCUMENT - https://developer.rimit.in/getting-started/readme#multi-tenant
 define('IS_MULTY_TENANT_PLATFORM', 'NO'); // OPTIONS - YES/NO
 define('MULTY_TENANT_MODE', 'QUERY'); // OPTIONS - QUERY/PARAMS
 
